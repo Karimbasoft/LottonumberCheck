698b80d938fea50ff42771cda1b87593f235ec3f
Robin Eckhardt <Robin99re@gmail.com>
2018-09-29 13:16:20 +0200

    Check in vor Umstellung auf LotteryTicketCollection

---
+++ App.Business/LotteryTicket/LotteryTicket.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Business.LotteryTicket
+{
+    public class LotteryTicket
+    {
+        #region Propertys
+        /// <summary>
+        /// The TicketNumber
+        /// </summary>
+        public int TicketNumber { get; set; }
+
+        /// <summary>
+        /// Collection of SparkleBoxes in the LotteryTicket
+        /// </summary>
+        public ObservableCollection<SparkleBox> SparkleBoxCollection { get; set; }
+
+        #endregion
+
+
+        public LotteryTicket(int number)
+        {
+            SparkleBoxCollection = new ObservableCollection<SparkleBox>();
+            TicketNumber = number;
+        }
+
+        public LotteryTicket(ObservableCollection<SparkleBox> sparkleBoxCollection)
+        {
+            SparkleBoxCollection = sparkleBoxCollection;
+        }
+
+        #region Methods
+        public async Task<bool> AddSparkleBoxToLotteryTicket(SparkleBox sparkleBox)
+        {
+            bool complete = false;
+
+            if (SparkleBoxCollection != null &&
+                SparkleBoxCollection.Count < 12 &&
+                sparkleBox.SparkleBoxNumbers.Length == 6)
+            {
+                SparkleBoxCollection.Add(sparkleBox);
+            }
+
+            return complete;
+        }
+        #endregion
+    }
+}
---
+++ App.Service/LotteryTicketService.cs
@@ -0,0 +1,88 @@
+﻿using App.Business.LotteryTicket;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Service
+{
+    public class LotteryTicketService
+    {
+        #region Fields
+        
+        #endregion
+        
+        #region Propertys
+        public ObservableCollection<LotteryTicket> LotteryTicketCollection { get; private set; }
+        #endregion
+
+        public LotteryTicketService()
+        {
+            ObservableCollection<LotteryTicket> LotteryTicket = new ObservableCollection<LotteryTicket>();
+        }
+
+        #region Methods
+        /// <summary>
+        /// Fügt einen Lottoschein der Liste hinzu
+        /// </summary>
+        /// <param name="lotteryTicket"></param>
+        /// <returns></returns>
+        public async Task<bool> AddLotteryTicketToCollection(LotteryTicket lotteryTicket)
+        {
+            bool complete = false;
+
+            if (lotteryTicket.SparkleBoxCollection != null &&
+                lotteryTicket.SparkleBoxCollection.Count < 12 &&
+                lotteryTicket.TicketNumber > 0)
+            {
+                LotteryTicketCollection.Add(lotteryTicket);
+            }
+
+            return complete;
+        }
+
+        /// <summary>
+        /// Entfernt ein Lottoschein aus der Liste
+        /// </summary>
+        /// <param name="lotteryTicket"></param>
+        /// <returns></returns>
+        public async Task<bool> RemoveLotteryTicketToCollection(LotteryTicket lotteryTicket)
+        {
+            bool complete = false;
+
+            if (LotteryTicketCollection.Any(l => l.TicketNumber == lotteryTicket.TicketNumber))
+            {
+                LotteryTicketCollection.Remove(
+                    LotteryTicketCollection.FirstOrDefault(l => l.TicketNumber == lotteryTicket.TicketNumber));
+                complete = true;
+            }
+
+            return complete;
+        }
+
+        /// <summary>
+        /// Updated ein Lottoschein aus der Liste
+        /// </summary>
+        /// <param name="lotteryTicket"></param>
+        /// <returns></returns>
+        public async Task<bool> UpdateLotteryTicketToCollection(LotteryTicket lotteryTicket)
+        {
+            bool complete = false;
+
+            if (LotteryTicketCollection.Any(l => l.TicketNumber == lotteryTicket.TicketNumber))
+            {
+                LotteryTicketCollection.Remove(
+                    LotteryTicketCollection.FirstOrDefault(l => l.TicketNumber == lotteryTicket.TicketNumber));
+
+                if(await AddLotteryTicketToCollection(lotteryTicket))
+                {
+                    complete = true;
+                }   
+            }
+            return complete;
+        }
+        #endregion
+    }
+}
